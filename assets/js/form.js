81b92a8ad7dcf5e22a894d47309a0db8e831c0c9
Silvia Via <S-via@github.com>
2024-07-22 23:57:06 -0700

    changed query selctor to select elemt by id scince i will only targeting #form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,6 @@
 
 // get values from each #id of username, title and content 
-const formElement = document.querySelector('form');
+const formElement = document.getElementById('#form');
 
  //const username = document.getElementById('username').value;
  //const title= document.getElementById('title').value;
@@ -11,27 +11,17 @@ const formElement = document.querySelector('form');
     const username = document.getElementById('username').value;
     const title= document.getElementById('title').value;
     const content= document.getElementById('content').value;
-console.log(username,title,content);
- }
-
-
-//if (username && title && content ) 
-
-//const newInput = {username,title,content};
-
-//input.push(newInput);
-
-
-
-// add newInput=JSON.stringfy()   to be able to store it into local.Storage
-
+    
+    console.log(username,title,content);
+ 
+}
+ //const newForm
+//__.push("newForm");
+//console.log(newForm);
+// add newInput =JSON.stringfy()  // to be able to store it into local.Storage
 //localStorage.()
 
+form.addEventListener('submit',submitForm);
+console.log(window.document);
 
-
-
-
-formElement.addEventListener('submit',submitForm);
-console.log("window.document:");
-console.log(window.document); 
 
