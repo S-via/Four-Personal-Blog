02d9ad39df093b090aa5cecf34619cc47417e404
Silvia Via <S-via@github.com>
2024-07-22 18:55:32 -0700

    console.log usranme , title, content

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,7 +1,37 @@
-const
+
+// get values from each #id of username, title and content 
+const formElement = document.querySelector('form');
+
+ //const username = document.getElementById('username').value;
+ //const title= document.getElementById('title').value;
+ //const content= document.getElementById('content').value;  // add all of these consts into a single variable and push into const newInput// //
+ 
+ function submitForm(event)
+ { event.preventDefault();
+    const username = document.getElementById('username').value;
+    const title= document.getElementById('title').value;
+    const content= document.getElementById('content').value;
+console.log(username,title,content);
+ }
+
+
+//if (username && title && content ) 
+
+//const newInput = {username,title,content};
+
+//input.push(newInput);
 
 
 
+// add newInput=JSON.stringfy()   to be able to store it into local.Storage
+
+//localStorage.()
+
+
+
+
+
+formElement.addEventListener('submit',submitForm);
 console.log("window.document:");
-console.log(window.document);
+console.log(window.document); 
 
