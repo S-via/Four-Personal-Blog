e3eadca20df2a04a57630a1afb730cd0037ae7f5
Silvia Via <S-via@github.com>
2024-07-24 16:31:20 -0700

    draft steps to add value in an object to store username, title, content push it to a spefic array to pull into local storage using parse and then store into localstorage using stringfy to link document.location to my blog html

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,6 +1,8 @@
 const sunshineSwitch = document.querySelector('#sunshine');
 const everything = document.querySelector('.everything');
 
+const blogArray= JSON.parse(localStorage.getItem('blogArray'))|| [];
+const blogContainer= document.querySelector(".blogcontainer")
 let mode ="light";
 
 sunshineSwitch.addEventListener('click' , function () {
@@ -14,4 +16,14 @@ sunshineSwitch.addEventListener('click' , function () {
         everything.setAttribute('class','light');}
     });
 
-
+    let blogCard=""
+for (let blog of blogArray){
+blogCard+= `
+<div class="blog-card">
+<h3>${blog.title}</h3>
+<p>${blog.username}</p>
+<p>${blog.content}</p>
+</div>
+`
+blogContainer.innerHTML=blogCard
+}
--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,6 @@
 
 // get values from each #id of username, title and content 
-const formElement = document.querySelector('#form');
+const formElement = document.querySelector('#form'); // query selector 
 
  //const username = document.getElementById('username').value;
  //const title= document.getElementById('title').value;
@@ -9,16 +9,27 @@ const formElement = document.querySelector('#form');
 
  function submitForm(event)
  { event.preventDefault();
+    const blogArray= JSON.parse(localStorage.getItem('blogArray'))|| []; // array to hold if the are localStorage and the [] is if there were not an array will be created  
+    const username = document.getElementById('username').value; // .value to get the value of element by id in html username
+    const title= document.getElementById('title').value; // .value to get the value of the elemment by id in html title 
+    const content= document.getElementById('content').value; // .vale to target the value of the element by id in html content
     
-    const username = document.getElementById('username').value;
-    const title= document.getElementById('title').value;
-    const content= document.getElementById('content').value;
-    
-    console.log(username,title,content);
+    const blogObject={
+        username: username, 
+        title: title,
+        content: content,
+    }
+blogArray.push(blogObject);
+localStorage.setItem('blogArray',JSON.stringify(blogArray))
  
+//username.value ="";
+//title.value="";
+//content.value="";
+ document.location.href="blog.html";
 }
 
 
+// the steps below is the draft i had to do 
  //const newForm {username,title,content};
 //__.push("empty");
 //console.log(newForm);
