aaca5d8ec19c004fc5d9a4285a51ab6e7eb03d58
Silvia Via <S-via@github.com>
2024-07-25 01:35:01 -0700

    added span inside input for blog.html to highlight the checkbox

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,8 +1,9 @@
 const sunshineSwitch = document.querySelector('#sunshine'); 
-const everything = document.querySelector('.everything'); 
+const everything = document.querySelector('.everything'); // queryselctor to target div class everything
 
-const blogArray= JSON.parse(localStorage.getItem('blogArray'))|| [];
-const blogContainer= document.querySelector(".blogcontainer")
+const blogArray= JSON.parse(localStorage.getItem('blogArray'))|| []; // get item from local storage from blog Array located in form.js
+const blogContainer= document.querySelector(".blogcontainer") // added array that containes in blog.html class .blogcontainer
+
 let mode ="light";
 
 sunshineSwitch.addEventListener('click' , function () {
